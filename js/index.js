8b6edf73e38cb852a45e945e294da25c08f37bce
Neha <nehachandra800@gmail.com>
2022-11-02 22:29:29 +0530

    adding documentation

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+// const mobile_nav = document.querySelector(".mobile-nav-btn");
+// const navbar_var = document.querySelector(".left-nav");
+
+// const toggleNavbar = () => {
+
+//   navbar_var.classList.toggle("active");
+// };
+
+// mobile_nav.addEventListener("click", () => toggleNavbar());
