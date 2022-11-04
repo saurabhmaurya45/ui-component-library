a0f767da54b5c549ee66906bd9c3ac494adeb95a
saurabhmaurya45 <tgtsaurabhmaurya45@gmail.com>
2022-11-05 02:28:54 +0530

    added responsiveness

--- js/index.js
+++ js/index.js
@@ -2,15 +2,41 @@
 var script = document.createElement('script');   
 script.src = './js/js/uiComponentLibrary.min.js'   
 document.head.appendChild(script)
+let navstate = true;
+let leftLststate = true;
 
 
-// const mobile_nav = document.querySelector(".mobile-nav-btn");
-// const navbar_var = document.querySelector(".left-nav");
+function mobileNavShow(){
+    let navbar = document.querySelector(".navbar");
+    let leftNav = document.querySelector(".left-nav");
+    let rightNav = document.querySelector(".right-nav");
+    if(leftLststate === false){
+        mobileLeftLstShow();
+    }
+    if (navstate===true){
+        navbar.style.height = "40vh";
+        leftNav.style.display = "flex";
+        rightNav.style.display = "flex";
+    }else{
+        leftNav.style.display = 'none';
+        rightNav.style.display = 'none';
+        navbar.style.height = "48px";
+    }
+    navstate = !navstate;
+}
 
-// const toggleNavbar = () => {
-
-//   navbar_var.classList.toggle("active");
-// };
-
-// mobile_nav.addEventListener("click", () => toggleNavbar());
+function mobileLeftLstShow(){
+    let item = document.querySelector(".left-section");
+    if(navstate===false){
+        mobileNavShow();
+    }
+    if(leftLststate === true){
+        item.style.visibility = 'visible';
+        // navstate = !navstate;
+    }
+    else{
+        item.style.visibility = 'hidden';
+    }
+    leftLststate = !leftLststate;
+}
 
